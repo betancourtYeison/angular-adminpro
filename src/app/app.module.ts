b50729367e12f06ec330aadd1f81d6c0f3ed5e14
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-06-01 12:20:10 -0500

    Modified modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,16 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
+import { PagesModule } from "./pages/pages.module";
 import { AppRoutingModule } from "./app-routing.module";
 
 import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./shared/header/header.component";
-import { SidebarComponent } from "./shared/sidebar/sidebar.component";
-import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
-import { PagesComponent } from "./pages/pages.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-import { ProgressComponent } from "./pages/progress/progress.component";
-import { Graphics1Component } from "./pages/graphics1/graphics1.component";
 import { LoginComponent } from "./login/login.component";
 import { RegisterComponent } from "./login/register.component";
-import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graphics1Component,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent
-  ],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule, PagesModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
