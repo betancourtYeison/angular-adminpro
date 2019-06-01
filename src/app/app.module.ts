bcfcbfa5c261a39db18375d625aa8e10173c70a7
Yeison Betancourt Solis <y.betancourt@adateceu.com>
2019-06-01 11:07:56 -0500

    Create project angular admin pro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { SidebarComponent } from "./shared/sidebar/sidebar.component";
+import { BreadcrumbsComponent } from "./shared/breadcrumbs/breadcrumbs.component";
+import { PagesComponent } from "./pages/pages.component";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { ProgressComponent } from "./pages/progress/progress.component";
+import { Graphics1Component } from "./pages/graphics1/graphics1.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./login/register.component";
+import { NopagefoundComponent } from "./shared/nopagefound/nopagefound.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graphics1Component,
+    LoginComponent,
+    RegisterComponent,
+    NopagefoundComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
